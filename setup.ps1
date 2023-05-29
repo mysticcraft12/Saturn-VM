2dbb94996a70b64c4d8db8400624ce9fe0c3812c
Lostzin.Mp4? <113808482+eulost@users.noreply.github.com>
2023-05-29 12:17:12 +0000

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,32 +1,3 @@
-# Desabilitar o firewall
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-
-# Baixar e executar o arquivo Bash
-$bashScript = @'
-#!/bin/bash
-
-url="https://download1980.mediafire.com/v08oehm7j8ogERS_wDNyD_VamvX45brTGrJhWWNj-HY1HMkcohKu2BjnFa6BCuN2uCBniqJWWlFbZoWNCYGKhoF7lq_8ZYizWuyPZANEnS0jEi3mE7A5i1cNeOtG0z9-fqv8aym19i8-9XmnlQze50oXa3EyxHp8WEF41ufh3gE/06hlixmv22padaq/Ninite+FileZilla+Notepad+TeamViewer+15+Installer.exe"
-output_file="Installer.exe"
-
-curl -L -o "$output_file" "$url"
-
-if [ $? -eq 0 ]; then
-  echo "Download concluído."
-else
-  echo "Falha ao baixar o arquivo."
-  exit 1
-fi
-
-chmod +x "$output_file"
-./"$output_file"
-'@
-
-# Salvar o código Bash em um arquivo temporário
-$bashScriptFile = [IO.Path]::GetTempFileName() + ".sh"
-$bashScript | Out-File -FilePath $bashScriptFile -Encoding ASCII
-
-# Executar o script Bash
-Start-Process -Wait -NoNewWindow bash -ArgumentList "-c '$bashScriptFile'"
-
-# Remover o arquivo temporário do script Bash
-Remove-Item -Path $bashScriptFile
+& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
