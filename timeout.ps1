71749ba4bddfe833d1a611f8f75a97880d946b18
Lostzin.Mp4? <113808482+eulost@users.noreply.github.com>
2023-10-25 10:00:02 -0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,6 +1,10 @@
+# Abre o Google no navegador padrão
+Start-Process "https://www.google.com"
+
+# Contagem regressiva
 $i = 360
 do {
     Write-Host $i
-    Sleep 360
+    Sleep 1
     $i--
 } while ($i -gt 0)
