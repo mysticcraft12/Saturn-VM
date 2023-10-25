5afc6d75f1d9e43a79b0888e6040678851f595a0
Lostzin.Mp4? <113808482+eulost@users.noreply.github.com>
2023-10-25 17:55:27 -0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,15 +1,15 @@
 # Caminho para o executável do Google Chrome
-$chromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+#$chromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
 # Abre o Google Chrome no site do Google
-Start-Process $chromePath -ArgumentList "https://download1478.mediafire.com/jks4l0rgv20gIZNe7NSkS3zJ_RA0D6iyOonVNxn2ZH2oZBwp6UE97_zcJkPbKBjWEUvA14K8u2AuK5vhT1xTACFhK-SvYo2WR5-sMxpXuZZ7eN3l75W5EReLqkNyi3OxvqSqf_Mv6zGQtCzHtptx0PRhmtYYO9FFKqgyw9eSLDib/7w9a6am4ymk9f4l/ninite.exe"
-Start-Sleep -Seconds 10  # Espera por 10 segundos (ajuste conforme necessário)
+#Start-Process $chromePath -ArgumentList "https://download1478.mediafire.com/jks4l0rgv20gIZNe7NSkS3zJ_RA0D6iyOonVNxn2ZH2oZBwp6UE97_zcJkPbKBjWEUvA14K8u2AuK5vhT1xTACFhK-SvYo2WR5-sMxpXuZZ7eN3l75W5EReLqkNyi3OxvqSqf_Mv6zGQtCzHtptx0PRhmtYYO9FFKqgyw9eSLDib/7w9a6am4ymk9f4l/ninite.exe"
+#Start-Sleep -Seconds 10  # Espera por 10 segundos (ajuste conforme necessário)
 
 # Caminho para o arquivo ninite.exe
-$ninitePath = "C:\Users\runneradmin\Downloads\ninite.exe"
+#$ninitePath = "C:\Users\runneradmin\Downloads\ninite.exe"
 
 # Executa o arquivo ninite.exe
-Start-Process $ninitePath
+#Start-Process $ninitePath
 
 # Contagem regressiva
 $i = 360
